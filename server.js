646baf9629a51037e2d26b289979d0e374135de5
Muhzinul Asrori <119827121+muhzinasrori@users.noreply.github.com>
2026-04-19 23:30:43 +0700

    server.js
    Update

--- server.js
+++ server.js
@@ -1,10 +1,107 @@
-const http = require('http');
+const readline = require('readline');
+const util = require('util');
 const fs = require('fs');
-const path = require('path');
 const os = require('os');
+const path = require('path');
+const http = require('http');
 
-const folderTujuan = 'E:\\coding\\HTML\\Pemutar Video\\';
+//membuat array untuk rl nya
+const rl = readline.createInterface({
+    input : process.stdin,
+    output : process.stdout
+});
 
+//--membuat pertanyaan rl.question yang mendukung await
+const question = util.promisify(rl.question).bind(rl);
+
+//--membuat fungsi yang async
+async function main() {
+    //--panggil fungsi pathQuestion
+    await pathQuestion();
+
+    //--Panggil fungsi portQuestion()
+    await portQuestion();
+
+    //--menghentikan rl.question supaya input terhenti
+    rl.close();
+
+    //--panggil fungsi untuk mengambil ipv4 pada komputer
+    getIP();
+
+    //--panggil fungsi untuk membuat server
+    createServer();
+}
+
+//--variabel penampung path
+let folderTujuan = '';
+
+//--membuat fungsi untuk input url html
+async function pathQuestion() {
+    console.log('===================================');
+    console.log('Masukkan folder path menggunakan Foward Slash (/)!');
+    console.log('Contoh : E:/coding/html/...');
+    const askPath = await question('Path folder html : ');
+
+    if (askPath === ''){
+        console.log(' ');
+        console.log('Info :')
+        console.log('-->Masukkan path!');
+        console.log(' ');
+        await pathQuestion();
+    } else if (!fs.existsSync(askPath)){
+        console.log(' ');
+        console.log('Info :')
+        console.log('-->Path tidak ditemukan!');
+        console.log('-->Masukkan Path dengan benar!');
+        console.log(' ');
+        await pathQuestion();
+    } else if (fs.existsSync(askPath)) {
+        folderTujuan = askPath;
+        console.log(' ');
+        console.log('Info :')
+        console.log(`-->OKE! Path di set ke ${folderTujuan}`);
+        console.log(' ');
+    }
+}
+
+//--variabel penampung port
+let PORT;
+
+//--membuat fungsi untuk logika input port
+async function portQuestion(){
+    console.log('===================================');
+    const askPort = 'Masukkan port : ';
+    const linkPort = await question(askPort);
+    let intger = Number(linkPort);
+
+    if (linkPort === ""){
+        PORT = 8080;
+        console.log(' ');
+        console.log('Info :')
+        console.log(`-->Port di set ke default : ${PORT}`)
+        console.log(' ');
+    } else if (isNaN(intger) && !Number.isInteger(intger)){
+        console.log(' ');
+        console.log('Info :')
+        console.log('-->Masukkan Angka!');
+        console.log(' ');
+        await portQuestion();
+    } else if (linkPort.length > 4 || linkPort.length < 4){
+        console.log(' ');
+        console.log('Info :')
+        console.log('-->Masukkan 4 angka saja!');
+        console.log(' ');
+        await portQuestion();
+    } else {
+        PORT = intger;
+        console.log(' ');
+        console.log('Info :')
+        console.log(`-->OKE! Port di set ke : ${PORT}`);
+        console.log(' ');
+    }
+}
+
+//--membuar variabel untuk server
 const server = http.createServer((req, res) => {
     //--header CORS
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -77,7 +174,7 @@ const server = http.createServer((req, res) => {
             case '.js':  contentType = 'text/javascript'; break;
             case '.png': contentType = 'image/png'; break;
             case '.jpg': contentType = 'image/jpg'; break;
-            case '.ttf': contentType = 'font/ttf'; break; // Tambahkan untuk font ikon kamu
+            case '.ttf': contentType = 'font/ttf'; break;
         }
 
         res.writeHead(200, { 'Content-Type': contentType });
@@ -85,41 +182,51 @@ const server = http.createServer((req, res) => {
     });
 });
 
-const port = 3000;
+//--variabel penampung info ipv4
+let ipv4Address = '';
 
-server.listen(port, '0.0.0.0', () => {
-    //--membuat variabel
+//--fungsi untuk mengambil info ipv4
+function getIP() {
     const networkInterfaces = os.networkInterfaces();
-    let localIp = '';
-
+    
     //--mencari alamat ipv4
     for(const interfacesName in networkInterfaces){
         const interfaces = networkInterfaces[interfacesName];
         for(const iface of interfaces){
             if(iface.family === 'IPv4' && !iface.internal){
-                localIp = iface.address;
+                ipv4Address = iface.address;
                 break;
             }
         }
-        if(localIp){
+        if(ipv4Address){
             break;
         }
     }
-
-    console.log('===================================');
-    console.log('Server Aktif!');
-    if(localIp){
-        console.log(`
-            Buka :
-            1. http://localhost:${port} untuk browser pada laptop ini.
-            2. http://${localIp}:${port} untuk browser pada perangkat lain.
-            3. Tekan ctrl + c untuk mengakiri server.
-        `);
-    } else{
-        console.log(`
-            Ipv4 tidak dapat ditemukan!
-            Buka http://localhost:${port} untuk browser pada laptop ini.
-            Tekan ctrl + c untuk mengakhiri server
-        `);
+    
+    if(!ipv4Address){
+        console.log('===================================');
+        console.log('IPv4 Address tidak ditemukan!');
+        console.log('Server hanya bisa dibuka di komputer ini.');
+        console.log(' ');
     }
-});
+}
+
+//--fungsi untuk membuat server
+function createServer(){
+    server.listen(PORT, '0.0.0.0', () => {
+        console.log('===================================');
+        console.log('Server Aktif!');
+        console.log('Buka browser untuk melihat server!');
+        console.log(' ');
+        console.log('Ketik :');
+        console.log(`1. http://localhost:${PORT} untuk melihat server pada browser di laptop`);
+        if(ipv4Address){
+            console.log(`2. http://${ipv4Address}:${PORT} untuk melihat server pada perangkat lain di jaringan yang sama`);
+        }
+        console.log('');
+        console.log('Tekan ctrl + c untuk mengakhiri server');
+    });
+}
+
+//--panggil fungsi main()
+main();
